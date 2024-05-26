c1ca0d0dc0e9d92ee4f7a9a6abb5e4cd0e6e4ebe
Basharat Ali <basharatalidogar14@gmail.com>
2024-05-26 17:15:25 +0500

    data added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,7 +13,30 @@ import CustomerSays from "../Utilities/4.png";
 import { IoIosStar } from "react-icons/io";
 import User from "../Utilities/user.jpg";
 import Footer from "../Layouts/Footer";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Petform from "../Utilities/pet-form.png"
 export default function Home() {
+    var settings = {
+        dots: false,
+        autoplay:true,
+        infinite: true,
+        speed: 700,
+        arrows:false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+             
+            }
+          },
+        ]
+      };
     return (
         <>
             <div className='container lg:px-28 flex flex-col md:flex-row py-16'>
@@ -25,7 +48,7 @@ export default function Home() {
                 </div>
                 <div className='md:w-1/2 lg:w-[70%] flex justify-center my-5 sm:my-0'><img src={HomeImg} className='w-[100%] md:w-[80%]' alt="my-img" /></div>
             </div>
-            <div className='md:px-20 lg:px-32 bg-[#ffebd6] flex flex-col md:flex-row gap-4 md:h-28 pb-10 my-10'> 
+            <div className='md:px-20 lg:px-32 bg-[#ffebd6] flex flex-col md:flex-row gap-4 md:h-28 pb-10 my-10'>
                 <div className='md:w-[40%] bg-transparent'><img src={SecondImg} className='-mt-[60px] mx-auto bg-transparent' alt="my-img" /></div>
                 <div className='md:w-[60%] lg:w-[50%] bg-transparent flex flex-col md:h-28 justify-center items-center sm:flex-row gap-4'>
                     <div className='flex flex-col bg-transparent sm:gap-1 items-center justify-center w-full'>
@@ -82,63 +105,95 @@ export default function Home() {
             {/* Best Selling Products */}
             <div className='container my-16 flex flex-col gap-10'>
                 <h1 className='text-center font-bold text-[25px] leading-[30px] md:text-[33px] md:leading-[40px] font-popp'>Our Best Selling Products</h1>
-                <div className='flex flex-wrap gap-3 justify-around'>
-                    <button className='text-[#ffebd6] px-5 lg:px-10 py-2 pb-[10px] rounded-md bg-[#fb6518] font-medium font-popp'>All Products</button>
-                    <button className='bg-[#ffebd6] px-5 text-[13px] md:text-[16px] lg:px-10 py-2 pb-[10px] duration-700 hover:text-[#ffebd6] hover:bg-[#fb6518] rounded-md text-[#fb6518] font-medium font-popp'>Dog Foods</button>
-                    <button className='bg-[#ffebd6] px-5 text-[13px] md:text-[16px] lg:px-10 py-2 pb-[10px] duration-700 hover:text-[#ffebd6] hover:bg-[#fb6518] rounded-md text-[#fb6518] font-medium font-popp'>Cat Foods</button>
-                    <button className='bg-[#ffebd6] px-5 text-[13px] md:text-[16px] lg:px-10 py-2 pb-[10px] duration-700 hover:text-[#ffebd6] hover:bg-[#fb6518] rounded-md text-[#fb6518] font-medium font-popp'>Cat Treats</button>
-                    <button className='bg-[#ffebd6] px-5 text-[13px] md:text-[16px] lg:px-10 py-2 pb-[10px] duration-700 hover:text-[#ffebd6] hover:bg-[#fb6518] rounded-md text-[#fb6518] font-medium font-popp'>Dog Treats</button>
-                    <button className='bg-[#ffebd6] px-5 text-[13px] md:text-[16px] lg:px-10 py-2 pb-[10px] duration-700 hover:text-[#ffebd6] hover:bg-[#fb6518] rounded-md text-[#fb6518] font-medium font-popp'>Pet Foods</button>
+                <div className='flex overflow-auto gap-3 justify-around'>
+                    <button className='flex items-center text-[#ffebd6] px-5 lg:px-10 py-2 pb-[10px] rounded-md bg-[#fb6518] font-medium font-popp'>AllProducts</button>
+                    <button className='flex items-center bg-[#ffebd6] px-5 text-[13px] md:text-[16px] lg:px-10 py-2 pb-[10px] duration-700 hover:text-[#ffebd6] hover:bg-[#fb6518] rounded-md text-[#fb6518] font-medium font-popp'>DogFoods</button>
+                    <button className='flex items-center bg-[#ffebd6] px-5 text-[13px] md:text-[16px] lg:px-10 py-2 pb-[10px] duration-700 hover:text-[#ffebd6] hover:bg-[#fb6518] rounded-md text-[#fb6518] font-medium font-popp'>CatFoods</button>
+                    <button className='flex items-center bg-[#ffebd6] px-5 text-[13px] md:text-[16px] lg:px-10 py-2 pb-[10px] duration-700 hover:text-[#ffebd6] hover:bg-[#fb6518] rounded-md text-[#fb6518] font-medium font-popp'>CatTreats</button>
+                    <button className='flex items-center bg-[#ffebd6] px-5 text-[13px] md:text-[16px] lg:px-10 py-2 pb-[10px] duration-700 hover:text-[#ffebd6] hover:bg-[#fb6518] rounded-md text-[#fb6518] font-medium font-popp'>DogTreats</button>
+                    <button className='flex items-center bg-[#ffebd6] px-5 text-[13px] md:text-[16px] lg:px-10 py-2 pb-[10px] duration-700 hover:text-[#ffebd6] hover:bg-[#fb6518] rounded-md text-[#fb6518] font-medium font-popp'>PetFoods</button>
                 </div>
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 '>
-                    <div className='bg-[#ffebd6] p-5 rounded-xl flex flex-col gap-1 relative shadow-2xl'>
-                        <img src={Pedigree} alt="my-img" className='bg-wite p-5 rounded-md' />
-                        <h3 className='bg-transparent text-[16px] md:text-[18px] font-semibold'>Sonima style BBQ</h3>
-                        <h4 className='bg-transparent text-[16px] md:text-[18px]'>All cat size</h4>
-                        <h5 className='bg-transparent text-[15px] md:text-[17px] font-bold'>$70.00</h5>
-                        <div className='absolute bottom-0 right-0 p-3 lg:p-5 rounded-br-xl bg-[#fb6518]'><BsCart className='text-white text-[22px] bg-transparent' /></div>
-                    </div>
-                    <div className='bg-[#ffebd6] p-5 rounded-xl flex flex-col gap-1 relative shadow-2xl'>
-                        <img src={Pedigree} alt="my-img" className='bg-wite p-5 rounded-md' />
-                        <h3 className='bg-transparent text-[16px] md:text-[18px] font-semibold'>Sonima style BBQ</h3>
-                        <h4 className='bg-transparent text-[16px] md:text-[18px]'>All cat size</h4>
-                        <h5 className='bg-transparent text-[15px] md:text-[17px] font-bold'>$70.00</h5>
-                        <div className='absolute bottom-0 right-0 p-3 lg:p-5 rounded-br-xl bg-[#fb6518]'><BsCart className='text-white text-[22px] bg-transparent' /></div>
-                    </div>
-                    <div className='bg-[#ffebd6] p-5 rounded-xl flex flex-col gap-1 relative shadow-2xl'>
-                        <img src={Pedigree} alt="my-img" className='bg-wite p-5 rounded-md' />
-                        <h3 className='bg-transparent text-[16px] md:text-[18px] font-semibold'>Sonima style BBQ</h3>
-                        <h4 className='bg-transparent text-[16px] md:text-[18px]'>All cat size</h4>
-                        <h5 className='bg-transparent text-[15px] md:text-[17px] font-bold'>$70.00</h5>
-                        <div className='absolute bottom-0 right-0 p-3 lg:p-5 rounded-br-xl bg-[#fb6518]'><BsCart className='text-white text-[22px] bg-transparent' /></div>
-                    </div>
-                </div>
+                <Slider className='flex gap-3 bg-transparent' {...settings}>
+                        <div className='bg-[#ffebd6] p-5 rounded-xl flex flex-col gap-1 relative shadow-2xl'>
+                            <img src={Pedigree} alt="my-img" className='bg-wite p-5 rounded-md' />
+                            <h3 className='bg-transparent text-[16px] md:text-[18px] font-semibold'>Sonima style BBQ</h3>
+                            <h4 className='bg-transparent text-[16px] md:text-[18px]'>All cat size</h4>
+                            <h5 className='bg-transparent text-[15px] md:text-[17px] font-bold'>$70.00</h5>
+                            <div className='absolute bottom-0 right-0 p-3 lg:p-5 rounded-br-xl bg-[#fb6518]'><BsCart className='text-white text-[22px] bg-transparent' /></div>
+                        </div>
+                        <div className='bg-[#ffebd6] p-5 rounded-xl flex flex-col gap-1 relative shadow-2xl'>
+                            <img src={Pedigree} alt="my-img" className='bg-wite p-5 rounded-md' />
+                            <h3 className='bg-transparent text-[16px] md:text-[18px] font-semibold'>Sonima style BBQ</h3>
+                            <h4 className='bg-transparent text-[16px] md:text-[18px]'>All cat size</h4>
+                            <h5 className='bg-transparent text-[15px] md:text-[17px] font-bold'>$70.00</h5>
+                            <div className='absolute bottom-0 right-0 p-3 lg:p-5 rounded-br-xl bg-[#fb6518]'><BsCart className='text-white text-[22px] bg-transparent' /></div>
+                        </div>
+                        <div className='bg-[#ffebd6] p-5 rounded-xl flex flex-col gap-1 relative shadow-2xl'>
+                            <img src={Pedigree} alt="my-img" className='bg-wite p-5 rounded-md' />
+                            <h3 className='bg-transparent text-[16px] md:text-[18px] font-semibold'>Sonima style BBQ</h3>
+                            <h4 className='bg-transparent text-[16px] md:text-[18px]'>All cat size</h4>
+                            <h5 className='bg-transparent text-[15px] md:text-[17px] font-bold'>$70.00</h5>
+                            <div className='absolute bottom-0 right-0 p-3 lg:p-5 rounded-br-xl bg-[#fb6518]'><BsCart className='text-white text-[22px] bg-transparent' /></div>
+                        </div>
+                    </Slider>
             </div>
             {/*Client says section */}
             <div className='container my-16'>
                 <div className='flex flex-col md:flex-row p-5 sm:p-10 lg:p-0 pb-0 pl-0 bg-[#ffebd6] rounded-xl'>
-                <div className='md:w-1/2 bg-transparent '><img src={CustomerSays} className='w-[100%] lg:w-[90%]' alt="" /></div>
-                <div className='md:w-1/2 p-5 sm:p-10 bg-transparent flex flex-col gap-1 md:gap-3 justify-center'>
-                    <h1 className=' text-[25px] leading-[30px] md:text-[35px] font-bold bg-transparent md:leading-[40px] font-popp'>What Our Client Says</h1>
-                    <p className='text-[13px] md:text-[15px] bg-transparent '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat dignissimos sapiente neque quam voluptatibus doloribus, ab obcaecati alias eius sit, eligendi repellat culpa officia, dolorem deleniti qui fugiat reprehenderit repellendus quae. Nisi, enim excepturi expedita fugit natus tenetur itaque ea.</p>
-                   <div className='bg-transparent flex gap-1'>
-                     <IoIosStar className='bg-transparent text-[15px] text-[#f96b40] md:text-[19px]'/>
-                     <IoIosStar className='bg-transparent text-[15px] text-[#f96b40] md:text-[19px]'/>
-                     <IoIosStar className='bg-transparent text-[15px] text-[#f96b40] md:text-[19px]'/>
-                     <IoIosStar className='bg-transparent text-[15px] text-[#f96b40] md:text-[19px]'/>
-                     <IoIosStar className='bg-transparent text-[15px] text-[#f96b40] md:text-[19px]'/>
-                   </div>
-                   <div className='bg-transparent flex items-center gap-1 mt-3'>
-                    <img src={User} className='w-[50px] h-[50px] rounded-full bg-transparent' alt="my-img" />
-                    <div className='bg-transparent'>
-                        <h3 className='bg-transparent text-[15px] md:text-[17px] font-bold'>Adelle Petricia</h3>
-                        <p className='bg-transparent text-[13px] md:text-[15px]'>CEO & Founder @Bellet</p>
+                    <div className='md:w-1/2 bg-transparent '><img src={CustomerSays} className='w-[100%] lg:w-[90%]' alt="" /></div>
+                    <div className='md:w-1/2 p-5 sm:p-10 bg-transparent flex flex-col gap-1 md:gap-3 justify-center'>
+                        <h1 className=' text-[25px] leading-[30px] md:text-[35px] font-bold bg-transparent md:leading-[40px] font-popp'>What Our Client Says</h1>
+                        <p className='text-[13px] md:text-[15px] bg-transparent '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat dignissimos sapiente neque quam voluptatibus doloribus, ab obcaecati alias eius sit, eligendi repellat culpa officia, dolorem deleniti qui fugiat reprehenderit repellendus quae. Nisi, enim excepturi expedita fugit natus tenetur itaque ea.</p>
+                        <div className='bg-transparent flex gap-1'>
+                            <IoIosStar className='bg-transparent text-[15px] text-[#f96b40] md:text-[19px]' />
+                            <IoIosStar className='bg-transparent text-[15px] text-[#f96b40] md:text-[19px]' />
+                            <IoIosStar className='bg-transparent text-[15px] text-[#f96b40] md:text-[19px]' />
+                            <IoIosStar className='bg-transparent text-[15px] text-[#f96b40] md:text-[19px]' />
+                            <IoIosStar className='bg-transparent text-[15px] text-[#f96b40] md:text-[19px]' />
+                        </div>
+                        <div className='bg-transparent flex items-center gap-1 mt-3'>
+                            <img src={User} className='w-[50px] h-[50px] rounded-full bg-transparent' alt="my-img" />
+                            <div className='bg-transparent'>
+                                <h3 className='bg-transparent text-[15px] md:text-[17px] font-bold'>Adelle Petricia</h3>
+                                <p className='bg-transparent text-[13px] md:text-[15px]'>CEO & Founder @Bellet</p>
+                            </div>
+                        </div>
                     </div>
-                   </div>
+                </div>
+            </div>
+             {/* contact page */}
+             <div className=' my-16 container'>
+               <div className='bg-[#ffebd6] p-5 rounded-xl shadow-xl md:p-10 flex flex-col md:flex-row'>
+                <div className='md:w-1/2 bg-transparent'>
+                    <img src={Petform} className='bg-transparent' alt="my-img" />
+                </div>
+                <div className='md:w-1/2 bg-transparent'>
+                    
+                <form className="flex flex-col gap-5 bg-transparent">
+            <div className="flex bg-transparent flex-col md:flex-row gap-3 md:gap-5 w-full ">
+                <div className="w-full bg-transparent">
+                    <label className='bg-transparent' for="">Name <span className="bg-transparent text-lg text-red-500">*</span></label> <br/>
+                    <input type="text" className="bg-transparent outline-none border border-gray-500 focus:border-green rounded-sm p-2 w-full"/>
+                </div>
+                <div className="w-full bg-transparent">
+                    <label className='bg-transparent' for="">E-Mail <span className="bg-transparent text-lg text-red-500">*</span></label> <br/>
+                    <input type="text" className="bg-transparent outline-none border border-gray-500 focus:border-green rounded-sm p-2 w-full"/>
+                </div>
+            </div>
+            <div className="w-full bg-transparent">
+                <label className='bg-transparent' for="">Subject <span className="bg-transparent text-lg text-red-500">*</span></label> <br/>
+                <input type="text" className="bg-transparent outline-none border border-gray-500 focus:border-green rounded-sm p-2 w-full"/>
+            </div>
+            <div className="w-full bg-transparent">
+                <label className="bg-transparent" for="">Your Message <span className="bg-transparent text-lg text-red-500">*</span></label> <br/>
+                <textarea name="" id="" cols="30" rows="5" className="bg-transparent outline-none border border-gray-500 focus:border-green rounded-sm p-2 w-full"></textarea> 
+            </div>
+            <Button2 btn2="Send Message" />
+        </form>
                 </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </>
     )
 }
